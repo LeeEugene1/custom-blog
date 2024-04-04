d071b61772fbf8a072ef8d7f1cb172a3f70c84bc
LeeEugene1 <uwm1004@gmail.com>
2024-04-04 18:33:46 +0900

    feat: layout, _app.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
+import Layout from '../../components/Layout';
 
 export default function FirstPost() {
-  return <div>첫번째 페이지입니다</div>;
+  return (
+    <Layout>
+      <Head>
+        <title>첫번째글</title>
+      </Head>
+      <h1>첫번째 페이지입니다</h1>
+    </Layout>
+  );
 }
